a96626b0298160ee496618807bcd254339f833d0
heav <hheav3@gmail.com>
2023-08-10 23:41:45 +0000

    i forgot to add sfx.lua to the last commit.

---
+++ src/sfx.lua
@@ -0,0 +1,36 @@
+local M = {}
+local sources = {}
+
+love.audio.setVolume(0.5)
+
+function M.load_sources()
+    local files = love.filesystem.getDirectoryItems("sound files")
+    for i, filename in ipairs(files) do
+        if #filename >= 4 then
+            local ending = filename:sub(#filename-3, #filename)
+            if ending == ".wav" or ending == ".ogg" or ending == ".mp3" then
+                local filestart = filename:sub(1, #filename-4)
+                sources[filestart] = {love.audio.newSource("sound files/"..filename, "static")}
+            end
+        end
+    end
+end
+
+function M.play(sourcename, volume)
+    volume = volume or 1
+    if not sources[sourcename] then error("missing a sound file: "..sourcename) end
+    for i, v in ipairs(sources[sourcename]) do
+        if not v:isPlaying() then
+            v:setVolume(volume)
+            v:play()
+            return
+        end
+    end
+    local new_source = sources[sourcename][1]:clone()
+    new_source:seek(0)
+    new_source:setVolume(volume)
+    new_source:play()
+    table.insert(sources[sourcename], new_source)
+end
+
+return M
